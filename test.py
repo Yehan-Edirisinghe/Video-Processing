d8cd95e58edc4333be9874d9d75a5c444d4c11c6
Yehan <y.edirisinghemudi@campus.unimib.it>
2023-12-20 23:54:39 +0100

    .

--- test.py
+++ test.py
@@ -1,34 +1,13 @@
-# import numpy as np
-# from threading import Thread
+import numpy as np
 
-# a = np.ones(10)
 
-# def func(b):
+def func(x):
 
-#     b[0] = 0
+    x[0] = 0
+    x[1] = 2
 
-#     return b
+a = np.ones(5)
 
-# x = Thread(target=func,args=(a))
+func(a)
 
-# print(x)
-
-def foo(bar, result, index):
-    print ('hello {0}'.format(bar))
-    result[index] = "foo"
-
-from threading import Thread
-
-threads = [None] * 10
-results = [None] * 10
-
-for i in range(len(threads)):
-    threads[i] = Thread(target=foo, args=('world!', results, i))
-    threads[i].start()
-
-# do some other stuff
-
-for i in range(len(threads)):
-    threads[i].join()
-
-print (" ".join(results))  # what sound does a metasyntactic locomotive make?
+print(a)
--- video.py
+++ video.py
@@ -1,11 +1,14 @@
 import cv2 as cv
 import numpy as np
-from threading import Thread
-import concurrent.futures as con
+from numpy.ctypeslib import as_ctypes
+
+import ctypes as c
+from multiprocessing import Process,RawArray
+
 import time
 
-path_input = r"C:\Users\Yehan\Documents\Python\Video-Processing\test.mp4"
-path_output = r'C:\Users\Yehan\Documents\Python\Video-Processing\output.avi'
+path_input  = r"C:\Users\Yehan\Documents\Python\Video-Processing\test.mp4"
+path_output = r"C:\Users\Yehan\Documents\Python\Video-Processing\output.avi"
 
 def render(cap,out):
 
@@ -29,18 +32,27 @@ def render(cap,out):
         if not ret:             # if frame is read correctly ret is True
             print("Can't receive frame (stream end?). Exiting ...")
             break
+        
+        tmp = np.zeros(frame.shape,dtype=np.uint8)
 
-        tmp = np.empty(frame.shape,dtype=np.uint8)
+        t = RawArray(c.c_uint8, (frame.shape[0]*frame.shape[1]*frame.shape[2]))
+        
+        t_np = np.frombuffer(t,dtype=np.uint8).reshape(frame.shape)
+        
+        np.copyto(t_np,tmp)
 
-        func(frame,oldFrame,tmp,range(frame_height),range(frame_width))
+        p1 = Process(target=func, args=(frame,oldFrame,t, range(0,int(frame_height/2)),range(frame_width)))
+        p1.start()
+        p1.join()
 
-        out.write(cv.cvtColor(tmp , cv.COLOR_BGR2GRAY))
+        # func(frame,oldFrame,tmp,range(frame_height),range(frame_width))
+
+        out.write(t_np)
 
         oldFrame[:] = frame
 
-        
         # cv.imshow('frame', cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
-        cv.imshow('frame', cv.cvtColor(tmp, cv.COLOR_BGR2GRAY))
+        cv.imshow('frame', cv.cvtColor(t_np, cv.COLOR_BGR2GRAY))
         
         counter += 1
 
@@ -53,10 +65,10 @@ def func(frame,oldFrame,tmp,xlim,ylim):
     for x in xlim:
             for y in ylim:
 
-                for i in range(3):
-                    
-                    tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
-                    # tmp[x,y,i] = frame[x,y,i]
+                # tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
+                tmp[x,y,0] = 255
+                tmp[x,y,1] = 255
+                tmp[x,y,2] = 0
 
     # return tmp
 
