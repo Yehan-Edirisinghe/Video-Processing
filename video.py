05de7f2e618b56bb54f2d029e0975dac9fef82bd
Yehan <y.edirisinghemudi@campus.unimib.it>
2023-12-20 16:28:05 +0100

    .

--- video.py
+++ video.py
@@ -4,8 +4,8 @@ from threading import Thread
 import concurrent.futures as con
 import time
 
-path_input = '/home/peppo/Documents/Video_processing/test.mp4'
-path_output = '/home/peppo/Documents/Video_processing/output.mp4'
+path_input = r"C:\Users\Yehan\Documents\Python\Video-Processing\test.mp4"
+path_output = r'C:\Users\Yehan\Documents\Python\Video-Processing\output.avi'
 
 def render(cap,out):
 
@@ -32,41 +32,16 @@ def render(cap,out):
 
         tmp = np.empty(frame.shape,dtype=np.uint8)
 
-        # k = 2
-        # threads = []
-
-        # for i in range(k):
-
-        #     rng = range(int((i)*frame_height/k),int((i+1)*frame_height/k))
-
-        #     # x = Process(target=func, args=(frame,oldFrame,tmp,rng,range(int(frame_width))),daemon=True)
-            
-        #     threads.append(x)
-
-        # for i in range(k):
-        #     threads[i].start()
-        #     threads[i].join()
-        
-        # with con.ThreadPoolExecutor() as executor:
-        #     futures = []
-
-        #     k = 2
-        #     for i in range(k):
-
-        #         rng = range(int((i)*frame_height/k),int((i+1)*frame_height/k))
-
-        #         x = executor.submit(target=func, args=(frame,oldFrame,tmp,rng,range(int(frame_width))),daemon=True)
-            
-        #         futures.append(x)
-
         func(frame,oldFrame,tmp,range(frame_height),range(frame_width))
 
-        out.write(tmp)
+        out.write(cv.cvtColor(tmp , cv.COLOR_BGR2GRAY))
 
         oldFrame[:] = frame
 
         
-        cv.imshow('frame', cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
+        # cv.imshow('frame', cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
+        cv.imshow('frame', cv.cvtColor(tmp, cv.COLOR_BGR2GRAY))
+        
         counter += 1
 
     cap.release()
@@ -80,8 +55,8 @@ def func(frame,oldFrame,tmp,xlim,ylim):
 
                 for i in range(3):
                     
-                    # tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
-                    tmp[x,y,i] = 0
+                    tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
+                    # tmp[x,y,i] = frame[x,y,i]
 
     # return tmp
 
@@ -92,7 +67,7 @@ if __name__ == '__main__':
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     
-    out = cv.VideoWriter(path_output,cv.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+    out = cv.VideoWriter(path_output,cv.VideoWriter_fourcc(*'MJPG'), 10, (frame_width,frame_height))
 
 
     a = time.time()
