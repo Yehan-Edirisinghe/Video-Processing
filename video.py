9ae62984215312ddfe9c5bc672a8aa3970226612
Yehan Edirisinghe <yedi278@gmail.com>
2023-12-20 17:01:18 +0100

    .

--- video.py
+++ video.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 from threading import Thread
+import concurrent.futures as con
+import time
 
 path_input = '/home/peppo/Documents/Video_processing/test.mp4'
 path_output = '/home/peppo/Documents/Video_processing/output.mp4'
@@ -12,7 +14,15 @@ def render(cap,out):
 
     oldFrame = np.zeros((frame_height,frame_width,3))
 
+    counter = 0
+
     while cap.isOpened():
+
+        if counter >= 20:
+            break
+
+        if cv.waitKey(1) == ord('q'):   # exit the program
+            break
 
         ret, frame = cap.read() 
 
@@ -20,34 +30,47 @@ def render(cap,out):
             print("Can't receive frame (stream end?). Exiting ...")
             break
 
+        tmp = np.empty(frame.shape,dtype=np.uint8)
 
-        tmp = np.zeros(frame.shape,dtype=np.uint8)
+        # k = 2
+        # threads = []
 
-        threads = list()
-        k = 2
-        for i in range(k):
+        # for i in range(k):
 
-            x = Thread(target=func, args=(frame,oldFrame,tmp,range(int((i+1)*frame_height/k)),range(int(frame_width))),daemon=True)
-            x.start()
-            threads.append(x)
+        #     rng = range(int((i)*frame_height/k),int((i+1)*frame_height/k))
 
-        for i in range(k):
-            threads[i].join()
+        #     # x = Process(target=func, args=(frame,oldFrame,tmp,rng,range(int(frame_width))),daemon=True)
+            
+        #     threads.append(x)
+
+        # for i in range(k):
+        #     threads[i].start()
+        #     threads[i].join()
         
+        # with con.ThreadPoolExecutor() as executor:
+        #     futures = []
+
+        #     k = 2
+        #     for i in range(k):
+
+        #         rng = range(int((i)*frame_height/k),int((i+1)*frame_height/k))
+
+        #         x = executor.submit(target=func, args=(frame,oldFrame,tmp,rng,range(int(frame_width))),daemon=True)
+            
+        #         futures.append(x)
+
+        func(frame,oldFrame,tmp,range(frame_height),range(frame_width))
+
         out.write(tmp)
 
         oldFrame[:] = frame
 
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        cv.imshow('frame', gray)
-
-        if cv.waitKey(1) == ord('q'):
-            break
-
+        
+        cv.imshow('frame', cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
+        counter += 1
 
     cap.release()
     cv.destroyAllWindows()
-    cap.release()
 
 
 def func(frame,oldFrame,tmp,xlim,ylim):
@@ -58,7 +81,7 @@ def func(frame,oldFrame,tmp,xlim,ylim):
                 for i in range(3):
                     
                     # tmp[x,y,i] = abs(oldFrame[x,y,i]-frame[x,y,i])
-                    tmp[x,y,i] = frame[x,y,i]
+                    tmp[x,y,i] = 0
 
     # return tmp
 
@@ -71,4 +94,8 @@ if __name__ == '__main__':
     
     out = cv.VideoWriter(path_output,cv.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 
+
+    a = time.time()
     render(cap,out)
+    b = time.time()
+    print(b-a)
